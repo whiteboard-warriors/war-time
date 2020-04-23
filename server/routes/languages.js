d39d8a1737748a49a2cf26cbdc40fb262fca2ba7
Pablo Motta <pmotta.dev@gmail.com>
2020-04-23 13:29:12 -0700

    get languages route

--- server/routes/languages.js
+++ server/routes/languages.js
@@ -6,7 +6,7 @@ const db = require('../models');
 // var isAuthenticatedData = require('../config/middleware/isAuthenticatedData');
 
 // @route   POST /add language
-// @desc    Admin creates an event
+// @desc    Admin can add a language
 
 router.post('/', async (req, res) => {
 	const { createdBy, language } = req.body;
@@ -30,27 +30,28 @@ router.post('/', async (req, res) => {
 	}
 });
 
-// @route   POST /add language
-// @desc    Admin creates an event
+// @route   GET / get languages
+// @desc    Admin can get all languages
 
-router.post('/add', async (req, res) => {
-	const {
-		// TODO
-	} = req.body;
+router.get('/', async (req, res) => {
 	try {
-		const language = new db.Language({
-			// TODO
-		});
-		await language.save();
-		res.send('Your language was added!');
+		// check to make sure user making updates has admin rights.
+		let user = await db.User.findOne({ _id: req.user._id });
+		if (user.admin !== true) {
+			return res.status(401).json({
+				msg: 'You are not authorized to perform this action.',
+			});
+		}
+		const languages = await db.Language.find();
+		res.send(languages);
 	} catch (err) {
 		console.error(err.message);
 		res.status(500).send('Server Error');
 	}
 });
 
-// @route   DELETE /:id - [works 2/12]
-// @desc    Delete language
+// @route   DELETE /:id -
+// @desc    Admin can delete languages
 
 router.delete('/:id', async (req, res) => {
 	try {
