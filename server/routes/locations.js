8813a46c68dbe6d68d6d2bcab9d637e240cbad68
Pablo Motta <pmotta.dev@gmail.com>
2020-04-23 15:00:26 -0700

    location update route

--- server/routes/locations.js
+++ server/routes/locations.js
@@ -65,24 +65,48 @@ router.post('/', async (req, res) => {
 	}
 });
 
-// // @route   UPDATE update/:id -
-// // @desc    Allows Admin to update location
+// @route   UPDATE /:id -
+// @desc    Allows Admin to update location
 
-// router.put('/update/:id', async (req, res) => {
-// 	const {
-// 		// TODO
-// 	} = req.body;
-// 	try {
-// 		const location = db.Location.findOne({
-// 			// TODO
-// 		});
-// 		await location.save();
-// 		res.send('Your location was updated!');
-// 	} catch (err) {
-// 		console.error(err.message);
-// 		res.status(500).send('Server Error');
-// 	}
-// });
+router.put('/:id', async (req, res) => {
+	const {
+		name,
+		street,
+		unit,
+		city,
+		state,
+		zipCode,
+		onlinePlatform,
+	} = req.body;
+	try {
+		// check to make sure user making updates has admin rights.
+		let user = await db.User.findOne({ _id: req.user._id });
+		if (user.admin !== true) {
+			return res.status(401).json({
+				msg: 'You are not authorized to make changes.',
+			});
+		}
+		const location = {};
+		if (name) location.name = name;
+		if (street) location.street = street;
+		if (unit) location.unit = unit;
+		if (city) location.city = city;
+		if (state) location.state = state;
+		if (zipCode) location.zipCode = zipCode;
+		if (onlinePlatform) location.onlinePlatform = onlinePlatform;
+		await db.Location.findOneAndUpdate(
+			{
+				_id: req.params.id,
+			},
+			{ $set: location }
+		);
+
+		res.send('Your location was updated!');
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
 
 // @route   DELETE /:id -
 // @desc    Deletes location by ID.
