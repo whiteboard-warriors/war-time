98bdf3ef231d368ced8607379ba75c6656bae215
Pablo Motta <pmotta.dev@gmail.com>
2020-04-23 14:45:57 -0700

    delete location route

--- server/routes/locations.js
+++ server/routes/locations.js
@@ -65,7 +65,7 @@ router.post('/', async (req, res) => {
 	}
 });
 
-// // @route   UPDATE update/:id - [works 2/12]
+// // @route   UPDATE update/:id -
 // // @desc    Allows Admin to update location
 
 // router.put('/update/:id', async (req, res) => {
@@ -84,20 +84,26 @@ router.post('/', async (req, res) => {
 // 	}
 // });
 
-// // @route   DELETE delete/:id - [works 2/12]
-// // @desc    Deletes location by ID.
+// @route   DELETE /:id -
+// @desc    Deletes location by ID.
 
-// router.delete('/delete/:id', async (req, res) => {
-// 	const { _id } = req.body;
-// 	try {
-// 		await db.Location.findOneAndDelete({
-// 			_id,
-// 		});
-// 		res.send('Your location was deleted!');
-// 	} catch (err) {
-// 		console.error(err.message);
-// 		res.status(500).send('Server Error');
-// 	}
-// });
+router.delete('/:id', async (req, res) => {
+	try {
+		// check to make sure user making updates has admin rights.
+		let user = await db.User.findOne({ _id: req.user._id });
+		if (user.admin !== true) {
+			return res.status(401).json({
+				msg: 'You are not authorized to make changes.',
+			});
+		}
+		await db.Location.findOneAndDelete({
+			_id: req.params.id,
+		});
+		res.send('The location was deleted!');
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
 
 module.exports = router;
