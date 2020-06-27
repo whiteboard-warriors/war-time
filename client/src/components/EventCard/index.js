c00094075ab5c72adbe723941387a7daadf19698
Pablo Motta <pmotta.dev@gmail.com>
2020-06-26 22:48:33 -0700

    basic markup for EventCard

--- client/src/components/EventCard/index.js
+++ client/src/components/EventCard/index.js
@@ -1,11 +1,62 @@
 import React from 'react';
 
+// import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+
 import './style.scss';
 
-const EventCard = () => {
+const EventCard = (props) => {
+	const {
+		image,
+		location,
+		date,
+		time,
+		signIn,
+		pair,
+		edit,
+		deleteEvent,
+	} = props;
 	return (
-		<div>
-			<h2>Event Card</h2>
+		<div className='card'>
+			<div className='img-container'>
+				<img src={image} alt='whiteboard warriors logo' />
+			</div>
+			<div className='info-container'>
+				<div className='info-left'>
+					<h3>Location</h3>
+					<p>{location}</p>
+					<h3>Date</h3>
+					<p>{date}</p>
+				</div>
+				<div className='info-right'>
+					<h3>Time</h3>
+					<p>{time}</p>
+					{pair && (
+						<Button variant='danger' size='md' onClick={pair}>
+							Pair
+						</Button>
+					)}
+					{edit && (
+						<Button variant='primary' size='md' onClick={edit}>
+							Edit
+						</Button>
+					)}
+					{deleteEvent && (
+						<Button
+							variant='primary'
+							size='md'
+							onClick={deleteEvent}
+						>
+							Delete
+						</Button>
+					)}
+					{signIn && (
+						<Button variant='primary' size='md' onClick={signIn}>
+							Signin
+						</Button>
+					)}
+				</div>
+			</div>
 		</div>
 	);
 };
