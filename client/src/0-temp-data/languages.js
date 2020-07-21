0c64e5f8c735c4c9f82254674e95fb4bb173a08d
Pablo Motta <pmotta.dev@gmail.com>
2020-07-20 21:29:21 -0700

    temp data

---
+++ client/src/0-temp-data/languages.js
@@ -0,0 +1,28 @@
+const languages = [
+	{
+		_id: '5ea1e561746ae4881d5f1dc6',
+		createdBy: '5e9cc061c7c28c2c3a26ce33',
+		language: 'Javascript',
+		__v: 0,
+	},
+	{
+		_id: '5ea1e561746ae4881d5f1dc6',
+		createdBy: '5e9cc061c7c28c2c3a26ce33',
+		language: 'Python/Ruby',
+		__v: 0,
+	},
+	{
+		_id: '5ea1e561746ae4881d5f1dc6',
+		createdBy: '5e9cc061c7c28c2c3a26ce33',
+		language: 'Java/Go',
+		__v: 0,
+	},
+	{
+		_id: '5ea1e561746ae4881d5f1dc6',
+		createdBy: '5e9cc061c7c28c2c3a26ce33',
+		language: 'C/C++',
+		__v: 0,
+	},
+];
+
+export default languages;
