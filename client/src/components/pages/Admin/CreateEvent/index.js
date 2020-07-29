5e52a204a88872d0bf746af9e791ed94ded24250
Pablo Motta <pmotta.dev@gmail.com>
2020-07-28 21:01:36 -0700

    create event page

---
+++ client/src/components/pages/Admin/CreateEvent/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.scss';
+
+const CreateEvent = () => {
+	return (
+		<div>
+			<div className='m-5'>Create Event</div>
+		</div>
+	);
+};
+
+export default CreateEvent;
