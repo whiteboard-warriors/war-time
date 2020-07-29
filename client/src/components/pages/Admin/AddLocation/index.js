1ce4f81ff3982c43ab1bc302951c58e796d02fb9
Pablo Motta <pmotta.dev@gmail.com>
2020-07-28 21:01:58 -0700

    add location page

---
+++ client/src/components/pages/Admin/AddLocation/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.scss';
+
+const AddLocation = () => {
+	return (
+		<div>
+			<div className='m-5'>Add Location</div>
+		</div>
+	);
+};
+
+export default AddLocation;
