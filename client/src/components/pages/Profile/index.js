44e88915dc96a7fa1b77084de25887a58b903dd2
Jordan Baucke <jordan.baucke@gmail.com>
2020-08-08 12:44:42 -0700

    formatting on profile

--- client/src/components/pages/Profile/index.js
+++ client/src/components/pages/Profile/index.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useContext, useState, useEffect } from 'react'
 import './style.scss'
-import { Form, Row, Col, Button } from 'react-bootstrap'
+import { Form, Row, Col, Button, Container } from 'react-bootstrap'
 import AuthContext from '../../../context/auth/authContext'
 import AlertContext from '../../../context/alert/alertContext'
 
@@ -34,10 +34,9 @@ const Profile = (props) => {
 	 *
 	 */
 	useEffect(() => {
-		if(updateProfileSuccess){
+		if (updateProfileSuccess) {
 			setAlert('Your profile has been updated.', 'success')
 		}
-
 	}, [updateProfileSuccess, props.history])
 
 	const onChangeProfile = (e) => {
@@ -59,206 +58,208 @@ const Profile = (props) => {
 
 	return (
 		<Fragment>
-			<h1 className="mt-5">
-				{user.firstName} {user.lastName}
-			</h1>
-			<Form onSubmit={saveProfile}>
-				<Row>
-					<Col>
-						<Form.Group controlId="formFirstname">
-							<Form.Label>First Name</Form.Label>
-							<Form.Control
-								type="text"
-								placeholder="What is your preferred name?"
-								name="firstName"
-								value={firstName}
-								onChange={onChangeProfile}
-								required
-							/>
-						</Form.Group>
-					</Col>
-					<Col>
-						<Form.Group controlId="formLastname">
-							<Form.Label>Last Name</Form.Label>
-							<Form.Control
-								type="text"
-								placeholder="What is your family/last name?"
-								name="lastName"
-								value={lastName}
-								onChange={onChangeProfile}
-								required
-							/>
-						</Form.Group>
-					</Col>
-				</Row>
-				<Form.Group controlId="formBasicPassword">
-					<Form.Label>Slack Username</Form.Label>
-					<Form.Control
-						type="text"
-						placeholder="Your Whiteboard Warriors Slack user"
-						onChange={onChangeProfile}
-						name="slackUsername"
-						value={slackUsername}
-					/>
-				</Form.Group>
-				<Row>
-					<Col>
-						<fieldset>
-							<Form.Group as={Row}>
-								<Form.Label as="legend" column sm={2}>
-									Primary Language
-								</Form.Label>
-								<Col sm={10} onChange={onChangeProfile}>
-									<Form.Check
-										type="radio"
-										label="JavaScript"
-										value="javascript"
-										name="primaryLanguage"
-										id="primarylanguageJavaScript"
-										checked={primaryLanguage === 'javascript'}
-										onChange={onChangeProfile}
-										disabled={secondaryLanguage === 'javascript'}
-									/>
-									<Form.Check
-										type="radio"
-										label="C/C++/Java/Go"
-										value="c-cplusplus-java-go"
-										name="primaryLanguage"
-										id="primarylanguageCJavaGo"
-										checked={primaryLanguage === 'c-cplusplus-java-go'}
-										onChange={onChangeProfile}
-										disabled={secondaryLanguage === 'c-cplusplus-java-go'}
-									/>
-									<Form.Check
-										type="radio"
-										label="Python/Ruby"
-										value="python-ruby"
-										name="primaryLanguage"
-										id="primarylanguagePythonRuby"
-										checked={primaryLanguage === 'python-ruby'}
-										onChange={onChangeProfile}
-										disabled={secondaryLanguage === 'python-ruby'}
-									/>
-								</Col>
+			<Container>
+				<h1 className="mt-5">
+					{user.firstName} {user.lastName}
+				</h1>
+				<Form onSubmit={saveProfile}>
+					<Row>
+						<Col>
+							<Form.Group controlId="formFirstname">
+								<Form.Label>First Name</Form.Label>
+								<Form.Control
+									type="text"
+									placeholder="What is your preferred name?"
+									name="firstName"
+									value={firstName}
+									onChange={onChangeProfile}
+									required
+								/>
 							</Form.Group>
-						</fieldset>
-					</Col>
-
-					<Col>
-						<fieldset>
-							<Form.Group as={Row}>
-								<Form.Label as="legend" column sm={2}>
-									Secondary Language
-								</Form.Label>
-								<Col sm={10}>
-									<Form.Check
-										type="radio"
-										label="JavaScript"
-										value="javascript"
-										name="secondaryLanguage"
-										id="secondarylanguageJavaScript"
-										checked={secondaryLanguage === 'javascript'}
-										onChange={onChangeProfile}
-										disabled={primaryLanguage === 'javascript'}
-									/>
-									<Form.Check
-										type="radio"
-										label="C/C++/Java/Go"
-										value="c-cplusplus-java-go"
-										name="secondaryLanguage"
-										id="secondarylanguageCJavaGo"
-										checked={secondaryLanguage === 'c-cplusplus-java-go'}
-										onChange={onChangeProfile}
-										disabled={primaryLanguage === 'c-cplusplus-java-go'}
-									/>
-									<Form.Check
-										type="radio"
-										label="Python/Ruby"
-										name="secondaryLanguage"
-										value="python-ruby"
-										id="secondarylanguagePythonRuby"
-										checked={secondaryLanguage === 'python-ruby'}
-										onChange={onChangeProfile}
-										disabled={primaryLanguage === 'python-ruby'}
-									/>
-								</Col>
+						</Col>
+						<Col>
+							<Form.Group controlId="formLastname">
+								<Form.Label>Last Name</Form.Label>
+								<Form.Control
+									type="text"
+									placeholder="What is your family/last name?"
+									name="lastName"
+									value={lastName}
+									onChange={onChangeProfile}
+									required
+								/>
 							</Form.Group>
-						</fieldset>
-					</Col>
-				</Row>
+						</Col>
+					</Row>
+					<Form.Group controlId="formBasicPassword">
+						<Form.Label>Slack Username</Form.Label>
+						<Form.Control
+							type="text"
+							placeholder="Your Whiteboard Warriors Slack user"
+							onChange={onChangeProfile}
+							name="slackUsername"
+							value={slackUsername}
+						/>
+					</Form.Group>
+					<Row>
+						<Col>
+							<fieldset>
+								<Form.Group as={Row}>
+									<Form.Label as="legend" column sm={2}>
+										Primary Language
+									</Form.Label>
+									<Col sm={10} onChange={onChangeProfile}>
+										<Form.Check
+											type="radio"
+											label="JavaScript"
+											value="javascript"
+											name="primaryLanguage"
+											id="primarylanguageJavaScript"
+											checked={primaryLanguage === 'javascript'}
+											onChange={onChangeProfile}
+											disabled={secondaryLanguage === 'javascript'}
+										/>
+										<Form.Check
+											type="radio"
+											label="C/C++/Java/Go"
+											value="c-cplusplus-java-go"
+											name="primaryLanguage"
+											id="primarylanguageCJavaGo"
+											checked={primaryLanguage === 'c-cplusplus-java-go'}
+											onChange={onChangeProfile}
+											disabled={secondaryLanguage === 'c-cplusplus-java-go'}
+										/>
+										<Form.Check
+											type="radio"
+											label="Python/Ruby"
+											value="python-ruby"
+											name="primaryLanguage"
+											id="primarylanguagePythonRuby"
+											checked={primaryLanguage === 'python-ruby'}
+											onChange={onChangeProfile}
+											disabled={secondaryLanguage === 'python-ruby'}
+										/>
+									</Col>
+								</Form.Group>
+							</fieldset>
+						</Col>
 
-				<Row>
-					<Col>
-						<fieldset>
-							<Form.Group as={Row}>
-								<Form.Label as="legend" column sm={2}>
-									Skill Level
-								</Form.Label>
-								<Col sm={10} onChange={onChangeProfile}>
-									<Form.Check
-										type="radio"
-										label="Beginner (Less than 1-year coding) "
-										name="skillLevel"
-										id="skillLevelAdvanced"
-										value="beginner"
-										checked={skillLevel === 'beginner'}
-									/>
-									<Form.Check
-										type="radio"
-										label="Easy (1-2 Years Coding)"
-										name="skillLevel"
-										id="skillLevelEasy"
-										value="easy"
-										checked={skillLevel === 'easy'}
-									/>
-									<Form.Check
-										type="radio"
-										label="Medium (2-5 years coding)"
-										name="skillLevel"
-										id="skillLevelMedium"
-										value="medium"
-										checked={skillLevel === 'medium'}
-									/>
-									<Form.Check
-										type="radio"
-										label="Hard (5+ years)"
-										name="skillLevel"
-										id="skillLevelHard"
-										value="hard"
-										checked={skillLevel === 'hard'}
-									/>
-								</Col>
-							</Form.Group>
-						</fieldset>
-					</Col>
-				</Row>
-				<Button variant="primary" type="submit">
-					Save
-				</Button>
-			</Form>
+						<Col>
+							<fieldset>
+								<Form.Group as={Row}>
+									<Form.Label as="legend" column sm={2}>
+										Secondary Language
+									</Form.Label>
+									<Col sm={10}>
+										<Form.Check
+											type="radio"
+											label="JavaScript"
+											value="javascript"
+											name="secondaryLanguage"
+											id="secondarylanguageJavaScript"
+											checked={secondaryLanguage === 'javascript'}
+											onChange={onChangeProfile}
+											disabled={primaryLanguage === 'javascript'}
+										/>
+										<Form.Check
+											type="radio"
+											label="C/C++/Java/Go"
+											value="c-cplusplus-java-go"
+											name="secondaryLanguage"
+											id="secondarylanguageCJavaGo"
+											checked={secondaryLanguage === 'c-cplusplus-java-go'}
+											onChange={onChangeProfile}
+											disabled={primaryLanguage === 'c-cplusplus-java-go'}
+										/>
+										<Form.Check
+											type="radio"
+											label="Python/Ruby"
+											name="secondaryLanguage"
+											value="python-ruby"
+											id="secondarylanguagePythonRuby"
+											checked={secondaryLanguage === 'python-ruby'}
+											onChange={onChangeProfile}
+											disabled={primaryLanguage === 'python-ruby'}
+										/>
+									</Col>
+								</Form.Group>
+							</fieldset>
+						</Col>
+					</Row>
 
-			<hr />
-			<h4>Change Password</h4>
-			<Form inline>
-				<Form.Label htmlFor="password">Password</Form.Label>
-				<Form.Control type="password" placeholder="Password" id="password" />
-				<Form.Label>Confirm Password</Form.Label>
-				<Form.Control type="password" placeholder="Password" />
-				<Button variant="primary" type="submit">
-					Reset
-				</Button>
-			</Form>
-			<hr />
-			<h4>Change E-Mail</h4>
-			<Form>
-				<Form.Group controlId="formGroupEmail">
-					<Form.Label>Email address</Form.Label>
-					<Form.Control type="email" placeholder="Enter email" />
+					<Row>
+						<Col>
+							<fieldset>
+								<Form.Group as={Row}>
+									<Form.Label as="legend" column sm={2}>
+										Skill Level
+									</Form.Label>
+									<Col sm={10} onChange={onChangeProfile}>
+										<Form.Check
+											type="radio"
+											label="Beginner (Less than 1-year coding) "
+											name="skillLevel"
+											id="skillLevelAdvanced"
+											value="beginner"
+											checked={skillLevel === 'beginner'}
+										/>
+										<Form.Check
+											type="radio"
+											label="Easy (1-2 Years Coding)"
+											name="skillLevel"
+											id="skillLevelEasy"
+											value="easy"
+											checked={skillLevel === 'easy'}
+										/>
+										<Form.Check
+											type="radio"
+											label="Medium (2-5 years coding)"
+											name="skillLevel"
+											id="skillLevelMedium"
+											value="medium"
+											checked={skillLevel === 'medium'}
+										/>
+										<Form.Check
+											type="radio"
+											label="Hard (5+ years)"
+											name="skillLevel"
+											id="skillLevelHard"
+											value="hard"
+											checked={skillLevel === 'hard'}
+										/>
+									</Col>
+								</Form.Group>
+							</fieldset>
+						</Col>
+					</Row>
 					<Button variant="primary" type="submit">
-						Update
+						Save
 					</Button>
-				</Form.Group>
-			</Form>
+				</Form>
+
+				<hr />
+				<h4>Change Password</h4>
+				<Form inline>
+					<Form.Label htmlFor="password">Password</Form.Label>
+					<Form.Control type="password" placeholder="Password" id="password" />
+					<Form.Label>Confirm Password</Form.Label>
+					<Form.Control type="password" placeholder="Password" />
+					<Button variant="primary" type="submit">
+						Reset
+					</Button>
+				</Form>
+				<hr />
+				<h4>Change E-Mail</h4>
+				<Form>
+					<Form.Group controlId="formGroupEmail">
+						<Form.Label>Email address</Form.Label>
+						<Form.Control type="email" placeholder="Enter email" />
+						<Button variant="primary" type="submit">
+							Update
+						</Button>
+					</Form.Group>
+				</Form>
+			</Container>
 		</Fragment>
 	)
 }
