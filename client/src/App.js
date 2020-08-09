ebfd969ff53ebae4e60d6e11210b9ce4c8aca974
Jordan Baucke <jordan.baucke@gmail.com>
2020-08-09 11:12:11 -0700

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,33 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Fragment } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 // Pages
-import Home from './components/pages/Home';
-import Landing from './components/pages/Landing';
-import Login from './components/pages/auth/Login';
-import Signup from './components/pages/auth/Signup';
-import Profile from './components/pages/Profile';
-import ForgotPassword from './components/pages/auth/ForgotPassword';
-import ResetPassword from './components/pages/auth/ResetPassword';
-import Event from "./components/pages/Event"
+import Home from './components/pages/Home'
+import Landing from './components/pages/Landing'
+import Login from './components/pages/auth/Login'
+import Signup from './components/pages/auth/Signup'
+import Profile from './components/pages/Profile'
+import ForgotPassword from './components/pages/auth/ForgotPassword'
+import ResetPassword from './components/pages/auth/ResetPassword'
+import Event from './components/pages/Event'
 //Admin Pages
-import Admin from './components/pages/Admin';
-import AddLanguage from './components/pages/Admin/AddLanguage';
-import AddLocation from './components/pages/Admin/AddLocation';
-import CreateEvent from './components/pages/Admin/CreateEvent';
+import Admin from './components/pages/Admin'
+import AddLanguage from './components/pages/Admin/AddLanguage'
+import AddLocation from './components/pages/Admin/AddLocation'
+import CreateEvent from './components/pages/Admin/CreateEvent'
 
 // import PrivateRoute from './components/routing/PrivateRoute';
 
-import Alerts from './components/Alerts';
-import Navbar from './components/Navbar';
-import EventState from './context/event/EventState';
-import AuthState from './context/auth/AuthState';
-import AlertState from './context/alert/AlertState';
-import setAuthToken from './utils/setAuthToken';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.scss';
+import Alerts from './components/Alerts'
+import Navbar from './components/Navbar'
+import EventState from './context/event/EventState'
+import AuthState from './context/auth/AuthState'
+import AlertState from './context/alert/AlertState'
+import setAuthToken from './utils/setAuthToken'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.scss'
 
 if (localStorage.token) {
-	setAuthToken(localStorage.token);
+	setAuthToken(localStorage.token)
 }
 
 const App = () => {
@@ -42,50 +42,46 @@ const App = () => {
 							<Switch>
 								<Route
 									exact
-									path='/landing'
+									path="/landing"
 									component={Landing}
 								/>
-								<Route exact path='/' component={Home} />
-								<Route exact path='/admin' component={Admin} />
+								<Route exact path="/" component={Home} />
+								<Route exact path="/admin" component={Admin} />
 								<Route
 									exact
-									path='/admin/create-event'
+									path="/admin/create-event"
 									component={CreateEvent}
 								/>
 								<Route
 									exact
-									path='/admin/add-language'
+									path="/admin/add-language"
 									component={AddLanguage}
 								/>
 								<Route
 									exact
-									path='/admin/add-location'
+									path="/admin/add-location"
 									component={AddLocation}
 								/>
+								<Route exact path="/event" component={Event} />
 								<Route
 									exact
-									path='/event/'
-									component={Event}
-								/>
-								<Route
-									exact
-									path='/signup'
+									path="/signup"
 									component={Signup}
 								/>
-								<Route exact path='/login' component={Login} />
+								<Route exact path="/login" component={Login} />
 								<Route
 									exact
-									path='/profile'
+									path="/profile"
 									component={Profile}
 								/>
 								<Route
 									exact
-									path='/forgot-password'
+									path="/forgot-password"
 									component={ForgotPassword}
 								></Route>
 								<Route
 									exact
-									path='/reset-password'
+									path="/reset-password"
 									component={ResetPassword}
 								></Route>
 							</Switch>
@@ -94,7 +90,7 @@ const App = () => {
 				</AlertState>
 			</EventState>
 		</AuthState>
-	);
-};
+	)
+}
 
-export default App;
+export default App
