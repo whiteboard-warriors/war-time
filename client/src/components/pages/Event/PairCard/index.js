25a392928f17e2471443c57f1da093dc9d1cb29a
Pablo Motta <pmotta.dev@gmail.com>
2020-09-06 13:46:56 -0700

    pair card - incomplete

---
+++ client/src/components/pages/Event/PairCard/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.scss';
+
+const PairCard = () => {
+	return (
+		<div className='my-3'>
+			<h4>Pair Card</h4>
+		</div>
+	);
+};
+
+export default PairCard;
