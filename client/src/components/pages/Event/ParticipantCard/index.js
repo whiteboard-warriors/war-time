8ffc960cd9f6aa908c262e527872a6a77ca2b59a
Pablo Motta <pmotta.dev@gmail.com>
2020-09-06 13:47:40 -0700

    participant card - incomplete

---
+++ client/src/components/pages/Event/ParticipantCard/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.scss';
+
+const ParticipantCard = () => {
+	return (
+		<div className='my-3'>
+			<h4>Participant Card</h4>
+		</div>
+	);
+};
+
+export default ParticipantCard;
