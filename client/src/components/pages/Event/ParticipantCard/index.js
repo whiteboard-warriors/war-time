c346e55aac70f19ea7fffed29b9ea5637f0cfd21
Pablo Motta <pmotta.dev@gmail.com>
2020-09-07 17:31:39 -0700

    removed infinite loop, fixed eslint error

--- client/src/components/pages/Event/ParticipantCard/index.js
+++ client/src/components/pages/Event/ParticipantCard/index.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AuthContext from '../../../../context/auth/authContext';
 
 import './style.scss';
 
 const ParticipantCard = () => {
+	const authContext = useContext(AuthContext);
+	const { isAuthenticated, user } = authContext;
+
+	const ucFirstLetter = (string) => {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	};
+
 	return (
-		<div className='my-3'>
-			<h4>Participant Card</h4>
+		<div className='participant-card-container'>
+			<div className='image'>
+				<i className='fa fa-user'></i>
+			</div>
+			<p>––</p>
+			<div className='user-name'>
+				<h5>
+					{user.firstName} {user.lastName}
+				</h5>
+			</div>
+			<p>––</p>
+			<div className='user-language'>
+				<p className='lead'>{ucFirstLetter(user.primaryLanguage)}</p>
+			</div>
 		</div>
 	);
 };
