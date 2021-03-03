3898aeee29d0b45778cf8d7c5f870068eb59b4e6
Andrew Wong <andrewzc93@gmail.com>
2021-03-02 22:54:42 -0800

    Fix navbar collapse, add empty job posting page ("get hired")

--- client/src/App.js
+++ client/src/App.js
@@ -1,133 +1,111 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // Bootstrap
-import { Container } from 'react-bootstrap';
+import { Container } from "react-bootstrap";
 // Pages
-import Home from './components/pages/Home';
-import Landing from './components/pages/Landing';
-import Login from './components/pages/auth/Login';
-import Signup from './components/pages/auth/Signup';
-import Profile from './components/pages/Profile';
-import ForgotPassword from './components/pages/auth/ForgotPassword';
-import ResetPassword from './components/pages/auth/ResetPassword';
-import Event from './components/pages/Event';
+import Home from "./components/pages/Home";
+import Landing from "./components/pages/Landing";
+import Login from "./components/pages/auth/Login";
+import Signup from "./components/pages/auth/Signup";
+import Profile from "./components/pages/Profile";
+import ForgotPassword from "./components/pages/auth/ForgotPassword";
+import ResetPassword from "./components/pages/auth/ResetPassword";
+import Event from "./components/pages/Event";
+import JobPosting from "./components/pages/JobPosting";
 //Admin Pages
-import Admin from './components/pages/Admin';
-import AddLanguage from './components/pages/Admin/AddLanguage';
-import AddLocation from './components/pages/Admin/AddLocation';
-import CreateEvent from './components/pages/Admin/CreateEvent';
-import EditEvent from './components/pages/Admin/EditEvent';
+import Admin from "./components/pages/Admin";
+import AddLanguage from "./components/pages/Admin/AddLanguage";
+import AddLocation from "./components/pages/Admin/AddLocation";
+import CreateEvent from "./components/pages/Admin/CreateEvent";
+import EditEvent from "./components/pages/Admin/EditEvent";
 //Private Route
 // import PrivateRoute from './components/routing/PrivateRoute';
 //Components
-import Alerts from './components/Alerts';
-import Navbar from './components/Navbar';
+import Alerts from "./components/Alerts";
+import Navbar from "./components/Navbar";
 //State
-import EventState from './context/event/EventState';
-import LocationState from './context/location/LocationState';
-import LanguageState from './context/language/LanguageState';
-import AuthState from './context/auth/AuthState';
-import AlertState from './context/alert/AlertState';
+import EventState from "./context/event/EventState";
+import LocationState from "./context/location/LocationState";
+import LanguageState from "./context/language/LanguageState";
+import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
 
 // Auth token
-import setAuthToken from './utils/setAuthToken';
+import setAuthToken from "./utils/setAuthToken";
 //Styles
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.scss';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
 
 if (localStorage.token) {
-	setAuthToken(localStorage.token);
+  setAuthToken(localStorage.token);
 }
 
 const App = () => {
-	return (
-		<AuthState>
-			<EventState>
-				<LocationState>
-					<LanguageState>
-						<AlertState>
-							<Router>
-								<Fragment>
-									<Navbar></Navbar>
-									<Container>
-										<Alerts />
-									</Container>
-									<Switch>
-										<Route
-											exact
-											path='/landing'
-											component={Landing}
-										/>
-										<Route
-											exact
-											path='/'
-											component={Home}
-										/>
-										<Route
-											exact
-											path='/event/:slug'
-											component={Event}
-										/>
-										<Route
-											exact
-											path='/admin'
-											component={Admin}
-										/>
-										<Route
-											exact
-											path='/admin/create-event'
-											component={CreateEvent}
-										/>
-										<Route
-											exact
-											path='/admin/edit-event/:id'
-											component={EditEvent}
-										/>
-										<Route
-											exact
-											path='/admin/add-language'
-											component={AddLanguage}
-										/>
-										<Route
-											exact
-											path='/admin/add-location'
-											component={AddLocation}
-										/>
+  return (
+    <AuthState>
+      <EventState>
+        <LocationState>
+          <LanguageState>
+            <AlertState>
+              <Router>
+                <Fragment>
+                  <Navbar></Navbar>
+                  <Container>
+                    <Alerts />
+                  </Container>
+                  <Switch>
+                    <Route exact path="/landing" component={Landing} />
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/event/:slug" component={Event} />
+                    <Route exact path="/admin" component={Admin} />
+                    <Route
+                      exact
+                      path="/admin/create-event"
+                      component={CreateEvent}
+                    />
+                    <Route
+                      exact
+                      path="/admin/edit-event/:id"
+                      component={EditEvent}
+                    />
+                    <Route
+                      exact
+                      path="/admin/add-language"
+                      component={AddLanguage}
+                    />
+                    <Route
+                      exact
+                      path="/admin/add-location"
+                      component={AddLocation}
+                    />
 
-										<Route
-											exact
-											path='/signup'
-											component={Signup}
-										/>
-										<Route
-											exact
-											path='/login'
-											component={Login}
-										/>
-										<Route
-											exact
-											path='/profile'
-											component={Profile}
-										/>
-										<Route
-											exact
-											path='/forgot-password'
-											component={ForgotPassword}
-										></Route>
-										<Route
-											exact
-											path='/reset-password'
-											component={ResetPassword}
-										></Route>
-									</Switch>
-								</Fragment>
-							</Router>
-						</AlertState>
-					</LanguageState>
-				</LocationState>
-			</EventState>
-		</AuthState>
-	);
+                    <Route exact path="/signup" component={Signup} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/profile" component={Profile} />
+                    <Route
+                      exact
+                      path="/forgot-password"
+                      component={ForgotPassword}
+                    ></Route>
+                    <Route
+                      exact
+                      path="/reset-password"
+                      component={ResetPassword}
+                    ></Route>
+                    <Route
+                      exact
+                      path="/job-posting"
+                      component={JobPosting}
+                    ></Route>
+                  </Switch>
+                </Fragment>
+              </Router>
+            </AlertState>
+          </LanguageState>
+        </LocationState>
+      </EventState>
+    </AuthState>
+  );
 };
 
 export default App;
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,97 +1,107 @@
-import React, { Fragment, useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import React, { Fragment, useContext } from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 // import moduleName from 'module';
-import './style.scss';
-import logo from './ww-logo.svg';
-import AuthContext from '../../context/auth/authContext';
+import "./style.scss";
+import logo from "./ww-logo.svg";
+import AuthContext from "../../context/auth/authContext";
 
 const Index = () => {
-	const authContext = useContext(AuthContext);
-	const { isAuthenticated, logout, user } = authContext;
+  const authContext = useContext(AuthContext);
+  const { isAuthenticated, logout, user } = authContext;
 
-	const authLinks = (
-		<Fragment>
-			<Link data-rb-event-key='/' className='nav-link' to='/'>
-				{' '}
-				Home
-			</Link>
+  const authLinks = (
+    <Fragment>
+      <Link data-rb-event-key="/" className="nav-link" to="/">
+        {" "}
+        Home
+      </Link>
 
-			{isAuthenticated && (
-				<NavDropdown
-					title={user.firstName ? user.firstName : ''}
-					id='collasible-nav-dropdown'
-				>
-					<Link
-						data-rb-event-key='/profile'
-						className='dropdown-item'
-						to='/profile'
-					>
-						Profile
-					</Link>
-					<Link
-						data-rb-event-key='/admin'
-						className='dropdown-item'
-						to='/admin'
-					>
-						Admin
-					</Link>
+      {isAuthenticated && (
+        <NavDropdown
+          title={user.firstName ? user.firstName : ""}
+          id="collasible-nav-dropdown"
+        >
+          <NavDropdown.Item
+            as={Link}
+            data-rb-event-key="/profile"
+            className="dropdown-item"
+            to="/profile"
+          >
+            Profile
+          </NavDropdown.Item>
+          <NavDropdown.Item
+            as={Link}
+            data-rb-event-key="/job-posting"
+            className="dropdown-item"
+            to="/job-posting"
+          >
+            Get Hired
+          </NavDropdown.Item>
+          <NavDropdown.Item
+            as={Link}
+            data-rb-event-key="/admin"
+            className="dropdown-item"
+            to="/admin"
+          >
+            Admin
+          </NavDropdown.Item>
+          <NavDropdown.Item
+            as={Link}
+            onClick={logout}
+            className="dropdown-item"
+            to="/landing"
+          >
+            Log Out
+          </NavDropdown.Item>
+        </NavDropdown>
+      )}
 
-					<Link
-						onClick={logout}
-						className='dropdown-item'
-						to='/landing'
-					>
-						Log Out
-					</Link>
-				</NavDropdown>
-			)}
+      <ul className="navbar-nav">
+        <li className="nav-item avatar">
+          <Link className="nav-link p-0" to="/profile">
+            <img
+              src="https://mdbootstrap.com/img/Photos/Avatars/avatar-5.jpg"
+              className="profile-pic rounded-circle z-depth-0"
+              alt="avatar"
+            ></img>
+          </Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 
-			<ul className='navbar-nav'>
-				<li className='nav-item avatar'>
-					<Link className='nav-link p-0' to='/profile'>
-						<img
-							src='https://mdbootstrap.com/img/Photos/Avatars/avatar-5.jpg'
-							className='profile-pic rounded-circle z-depth-0'
-							alt='avatar'
-						></img>
-					</Link>
-				</li>
-			</ul>
-		</Fragment>
-	);
+  const guestLinks = (
+    <Fragment>
+      <Link data-rb-event-key="/login" className="nav-link" to="/login">
+        Login
+      </Link>
+      <Link data-rb-event-key="/signup" className="nav-link" to="/signup">
+        Signup
+      </Link>
+    </Fragment>
+  );
 
-	const guestLinks = (
-		<Fragment>
-			<Link data-rb-event-key='/login' className='nav-link' to='/login'>
-				Login
-			</Link>
-			<Link data-rb-event-key='/signup' className='nav-link' to='/signup'>
-				Signup
-			</Link>
-		</Fragment>
-	);
-
-	return (
-		<Navbar expand='lg'>
-			<Link className='navbar-brand' to='/'>
-				War{' '}
-				<img
-					alt='whiteboard warriors - war time'
-					className='nav-logo'
-					src={logo}
-				></img>{' '}
-				Time
-			</Link>
-			<Navbar.Toggle aria-controls='basic-navbar-nav' />
-			<Navbar.Collapse id='basic-navbar-nav'>
-				<Nav className='ml-auto'>
-					{isAuthenticated ? authLinks : guestLinks}
-				</Nav>
-			</Navbar.Collapse>
-		</Navbar>
-	);
+  return (
+    <Navbar expand="lg">
+      <Link className="navbar-brand" to="/">
+        War{" "}
+        <img
+          alt="whiteboard warriors - war time"
+          className="nav-logo"
+          src={logo}
+        ></img>{" "}
+        Time
+      </Link>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          {isAuthenticated ? authLinks : guestLinks}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default Index;
